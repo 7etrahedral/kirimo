f3d67b339717ff4950f4a0a9526467ded2f888de
Ogilvy Digital <wibowo.bullseye@gmail.com>
2016-06-30 16:06:29 +0800

    new class

--- src/test/java/TestLoginLogout.java
+++ src/test/java/TestLoginLogout.java
@@ -1,7 +1,11 @@
-import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 import java.io.File;
 import java.net.MalformedURLException;
@@ -9,15 +13,15 @@ import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 /**
- * Created by wib_ on 6/25/16.
+ * Created by wib_ on 6/30/16.
  */
-public class AndroidSetup {
+public class TestLoginLogout {
 
     protected AndroidDriver ad;
     protected WebDriverWait wait;
 
-    protected void androidSetUp() throws MalformedURLException{
-//        File appDir = new File("/Users/genta/Documents/Job/Kirimo/apk");
+    @BeforeClass
+    public void setUp() throws MalformedURLException {
         File appDir = new File("/Users/ogilvydigital/Documents/apk");
         File app = new File(appDir, "kirimo-V.1.0.apk");
 
@@ -38,6 +42,17 @@ public class AndroidSetup {
         wait = new WebDriverWait(ad, 10);
 
         ad.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    }
+
+    @AfterClass
+    public void tearDown() {
+        ad.quit();
+    }
+
+    @Test
+    public void verifyInstruction() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.kirimo.driver:id/textViewLoginDesc")));
+//        loginpage.getInstructionText();
     }
 
 }
